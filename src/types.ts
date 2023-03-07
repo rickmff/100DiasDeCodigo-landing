f57a542dc9290ea215519579e27ad8f02d75225d
rickmff <rickmff@gmail.com>
2023-03-07 03:15:37 -0300

    lading page 100 dias de codigo

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export interface CompatibilityItem {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface BeneficiosItem {
+  description: string;
+  icon: string;
+}
+
+export interface FooterLink {
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface NavItem {
+  title: string;
+  url: string;
+}
+
+export interface ShowcaseSite {
+  title: string;
+  image: ImageMetadata;
+  url: string;
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+      },
+      backgroundColor: {
+        primary: "var(--color-background-primary)",
+        secondary: "var(--color-background-secondary)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
+    },
+  },
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require("tailwindcss-fluid-type")],
+};
