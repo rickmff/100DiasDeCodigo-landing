d0c9aa4aa5610039c3bc7bbddcdd988d18c854f5
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2023-10-24 21:41:08 -0300

    fix: use site property from astro.config to fix embed image is not showing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.100diasdecodigo.dev/",
   integrations: [
     tailwind(),
     image({
